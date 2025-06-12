93106a3fb15d30e9db53876a7f797492170ac43a
RenzoQuispe <renzo123cd@gmail.com>
2025-06-12 17:19:39 -0500

    actualizar auth.route.js

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,18 +1,16 @@
 import { Router } from "express"; // para crear un enrutador
 import { register, login, logout, profile, verifyToken } from "../controllers/auth.controller.js";
 import { authRequired } from "../middlewares/validateToken.js";
-import { validateSchema} from "../middlewares/validator.middleware.js";
-import { registerSchema, loginSchema } from "../schemas/auth.schema.js";
 
 
-const router = Router();    
+const router = Router();
 
-router.post('/register', validateSchema(registerSchema) ,register); // peticion post en http://localhost:4000/api/register (respuesta register)
-router.post('/login', validateSchema(loginSchema),login); // post: Enviar datos al servidor para crear o modificar recursos.
+router.post('/register', register);
+router.post('/login', login);
 router.get('/verify', verifyToken);
 
 router.post('/logout', logout);
-router.get('/profile', authRequired, profile); //get:Obtener información del servidor.
+router.get('/profile', authRequired, profile);
 
 
 export default router;
